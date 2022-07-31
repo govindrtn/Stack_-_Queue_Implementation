c63a0a5e52e537d1408a59b53a7e14b077200f1d
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-31 10:46:43 +0530

    Added : pop and peak method calling from linkedList

--- StackServices.java
+++ StackServices.java
@@ -2,14 +2,24 @@ package com.bridgelabz.stack;
 
 import com.bridgelabz.linkedlist.LinkedListService;
 
-public class StackServices <T extends Comparable <T>> {
-	
+public class StackServices<T extends Comparable<T>> {
+
 	LinkedListService<T> linkedList = new LinkedListService<T>();
-	
+
 	public void push(T data) {
 		linkedList.add(data);
 	}
+	
+	public void pop() {
+		linkedList.pop();
+	}
+	
+	public void peak() {
+		System.out.println("Top element in linkedLit "+linkedList.head.data);
+	}
+
 	public void print() {
 		linkedList.display();
 	}
+	
 }
